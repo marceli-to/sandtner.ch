09a1e8176faa5fc0d2fa8ed73b6f9c834ee60df0
Marcel Stadelmann <m@marceli.to>
2024-12-19 14:02:28 +0100

    wip

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,6 @@
 @props(['url'])
 <tr>
   <td class="header">
-    <img src="{{ config('app.url') }}/img/logo-carrosserie-sandtner.png" height="512" width="512" style="display:block; height:auto; max-width: 150px !important; width: 100%;">
+    <img src="{{ config('app.url') }}/img/logo-carrosserie-sandtner.png" height="512" width="512" style="display:block; height:auto; max-width: 100px !important; width: 100%;">
   </td>
 </tr>
